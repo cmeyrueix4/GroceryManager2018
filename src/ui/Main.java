461be4b8f34018139a8b00cdafcd534896385cc0
Cyrielle Meyrueix <cmeyrueix44@gmail.com>
2018-09-10 16:28:48 -0700

    Created a main class and one method

---
+++ src/ui/Main.java
@@ -0,0 +1,11 @@
+package ui;
+
+public class Main {
+    public static void main(String[] args) {
+        foodName();
+    }
+    public static void foodName(){
+        System.out.println("Food");
+    }
+
+}
