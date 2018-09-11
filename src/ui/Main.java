b8fea761f526b748e0fb30ada1cec9e18c0f4d0c
Cyrielle Meyrueix <cmeyrueix44@gmail.com>
2018-09-10 19:36:13 -0700

    Created a method called foodType()

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,9 +3,13 @@ package ui;
 public class Main {
     public static void main(String[] args) {
         foodName();
+        foodType();
     }
     public static void foodName(){
         System.out.println("Food");
+    }
+    public static void foodType(){
+        System.out.println("Dairy");
     }
 
 }
